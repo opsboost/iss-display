56a66f804a4472ecffce0562bfeb475282a23a0a
Björn Busse <bj.rn@baerlin.eu>
2026-08-23 00:50:01 +0200

    entrypoint: Generate a machine-id, quiet the firefox dbus warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -o errexit
 
+if [ ! -s /etc/machine-id ]; then
+    read -r uuid < /proc/sys/kernel/random/uuid
+    printf '%s\n' "${uuid//-/}" > /etc/machine-id
+fi
+
 case "$1" in
     sh|bash)
         set -- "$@"
